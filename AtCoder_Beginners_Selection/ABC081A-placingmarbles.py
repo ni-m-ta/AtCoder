5c7db33f36165c712a9939d555eb95b8d4668877
nita <tatsuyanisato@gmail.com>
2022-06-28 01:50:03 +0900

    done

---
+++ AtCoder_Beginners_Selection/ABC081A-placingmarbles.py
@@ -0,0 +1,6 @@
+s=input()
+count=0
+for i in range(len(s)):
+    count+=int(s[i])
+
+print(count)
---
+++ AtCoder_Beginners_Selection/ABC086A-product.py
@@ -0,0 +1,7 @@
+a,b=map(int,input().split())
+pro=a*b
+
+if pro%2==0:
+    print('Even')
+else:
+    print('Odd')
