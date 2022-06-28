d0de9f7dd3f678a9cbb50e8034ffef056bbe7bf1
apple <user@NiTanoMacBook-Pro.local>
2022-06-28 14:12:16 +0900

    done

--- problems_sortedby_dreken.py/ABC075B-mine.py
+++ problems_sortedby_dreken.py/ABC075B-mine.py
@@ -9,9 +9,24 @@ for i in range(h):
     for ii in range(w):
         count=0
         if str_list[i][ii]=='.':
-            count+=str_list[i][ii-1:ii+2].count('#')
-            mine_list.append(count)
+            if ii==0:
+                if i!=0:
+                    count+=str_list[i-1][ii:ii+2].count('#')
+                count+=str_list[i][ii:ii+2].count('#')
+                if i!=(h-1):
+                    count+=str_list[i+1][ii:ii+2].count('#')
+                mine_list.append(count)
+            else:
+                if i!=0:
+                    count+=str_list[i-1][ii-1:ii+2].count('#')
+                count+=str_list[i][ii-1:ii+2].count('#')
+                if i!=(h-1):
+                    count+=str_list[i+1][ii-1:ii+2].count('#')
+                mine_list.append(count)
         else:
             mine_list.append(str_list[i][ii])
 
-print(mine_list)
+for i in range(1,h*w+1):
+    print(mine_list[i-1],end='')
+    if i%w==0:
+        print('')
